0c2a0ec5539f1d8d3610bd0c1003e9b11aff3183
Moreen <moreencharles@gmail.com>
2023-11-14 00:46:26 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import ProjectList from './ProjectList';
 
-function App() {
+const projectsData = [
+  {
+    id: 1,
+    technologies: ['React', 'JavaScript', 'CSS'],
+  },
+  {
+    id: 2,
+    technologies: ['Node.js', 'Express', 'MongoDB'],
+  },
+  // Add more projects as needed
+];
+
+const App = () => {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projectsData} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,19 @@
-import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+import React from 'react';
+
+const ProjectItem = ({ technologies }) => {
   return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+    <div>
+      <h3>Project Title</h3>
+      <p>Description of the project.</p>
+      <h4>Technologies Used:</h4>
+      <div>
+        {technologies.map((technology, index) => (
+          <span key={index}>{technology}</span>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default ProjectItem;
